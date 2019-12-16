a64244f5c8e21096fcfb2c6dcb55628b672d0943
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 09:53:02 -0700

    Add cin and namespace

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout <<"Hello Again C++" <<std::endl;
+    int age;    //int: signed integers
+    //cout: for system output
+    std::cout <<"Hello Again C++" << std::endl;
+    cout <<"Another line under namespace std" << endl;
+    cout <<"Wildcats "<<"WSU "<<"Ogden "<<endl;
+    //cin: for system / user input
+    cout << "What is your age?" <<endl;
+    cin >> age;
+    cout << "I see you are " << age << endl;
     return 0;
 }
