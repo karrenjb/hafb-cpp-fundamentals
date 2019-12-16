e2ed2cb8909d118d115a783786453bc1615d70f0
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 16:32:12 -0700

    Functions

---
+++ Module1/LA1-2/functions.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file functions.cpp
+ * @author your name (you@domain.com)
+ * @brief First look at functions
+ * @version 0.1
+ * @date 2019-12-16
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <array>
+
+using namespace std;
+#include "functions.h"  //** are for local headers
+
+
+// Function Definition
+void Hello()
+{
+    cout << "Hello from Hello() function" <<endl;
+}
