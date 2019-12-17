5d166b06f06700218b50008a4b224c53667b5c1e
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-17 14:09:38 -0700

    Cars

--- Module1/TO1-5/src/cars.cpp
+++ Module1/TO1-5/src/cars.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 #include "cars.h"
-// using namespace lamborghini;
-// using namespace porsche;
+
+int HorsePower()    // "standard" namespace
+{
+    return 150;
+}   
+
+int lamborghini::HorsePower()   // using namespace lamborghini;
+{
+    return 750;
+}   
+
+void lamborghini::cout()
+{
+    std::cout << "Super Car " << std::endl;
+}
+
+int porsche::HorsePower()   // using namespace porsche;
+{
+    return 700;
+}   
--- Module1/TO1-5/src/cars.h
+++ Module1/TO1-5/src/cars.h
@@ -1,5 +1,17 @@
 #ifndef CARS_H_
 #define CARS_H_
 
+int HorsePower();   // "nameless, :: " namespace
+
+namespace lamborghini
+{
+    int HorsePower();   //Inside Lamborghini namespace
+    void cout();
+}
+
+namespace porsche
+{
+    int HorsePower();   //Inside Porsche namespace
+}
 
 #endif /* !CARS_H_ */
