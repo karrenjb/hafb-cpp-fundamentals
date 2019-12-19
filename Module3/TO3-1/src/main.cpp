f588499c7d7b0653c120e914739b672c1e67b915
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-19 10:29:35 -0700

    Operator - and update_distance

--- Module3/TO3-1/src/main.cpp
+++ Module3/TO3-1/src/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include "distance.h"
-using namespace std;
+// using namespace std;
 
 // Main Function
 int main() 
 {
+
+  Distance d1 (34, 9.5);
+  
+  d1.ShowDist();
+
+  Distance d2(10, 8.5);
+  d2.ShowDist();
+
+  Distance d3;
+  d3 = d1 + d2;
+  d3.ShowDist();
+
+  Distance d4;
+  d4 = d1 + d2 + d3;
+  d4.ShowDist();
+
+  //Display int with stdout
+  std::cout << d4 << std::endl;
+
+  Distance d5;
+  d5 = d4 - d3;
+  d5.ShowDist();
+    
   return 0;
 }
